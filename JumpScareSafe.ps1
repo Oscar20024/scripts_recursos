1f5414d5d6c4720891b1e9320bdc20ab85b79810
Oscar20024 <oscoscantonio@gmail.com>
2026-07-12 21:16:56 -0500

    Update JumpScareSafe.ps1

--- JumpScareSafe.ps1
+++ JumpScareSafe.ps1
@@ -34,7 +34,7 @@ $wavUrl = "https://raw.githubusercontent.com/Oscar20024/scripts_recursos/main/sc
 
 
 # Tiempo que espera antes de mostrar el efecto
-$delayBeforeEffect = 5
+$delayBeforeEffect = 0.5
 
 
 # Tiempo que la imagen permanecerá en pantalla
